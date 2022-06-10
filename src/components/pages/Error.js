7f1f3aaa82c2fc1837116c4aba27c0c00c22a841
Deniska <47465987+deniska69@users.noreply.github.com>
2022-06-10 15:07:07 +0700

    Исправленна кнопка для возврата на главную страница на странице 404

--- src/components/pages/Error.js
+++ src/components/pages/Error.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Container, Heading, Box, Link, Button } from '@chakra-ui/react';
+import { Container, Heading, Box, Button } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 
 function Error() {
   return (
@@ -8,7 +9,7 @@ function Error() {
         Ошибка 404 - такой страницы не существует! :с
       </Heading>
       <Box align="center" my={10}>
-        <Link href="/" variant="hover-no">
+        <Link to="/" variant="hover-no">
           <Button colorScheme="teal">Вернуться на главную страницу</Button>
         </Link>
       </Box>
