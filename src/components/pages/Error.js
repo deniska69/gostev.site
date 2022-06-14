e04e9e7d0cb74f78156ad3324cc4f840aa18c8b0
Deniska <47465987+deniska69@users.noreply.github.com>
2022-06-15 01:40:43 +0700

    Для страницы 404 добавлена анимация отображения текста и кнопки, а также добавлен отступ сверху

--- src/components/pages/Error.js
+++ src/components/pages/Error.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { Container, Heading, Box, Button } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
+import Section from '../Section';
 
 function Error() {
   return (
-    <Container>
-      <Heading as="h3" fontSize={20} mb={4}>
-        Ошибка 404 - такой страницы не существует! :с
-      </Heading>
-      <Box align="center" my={10}>
-        <Link to="/" variant="hover-no">
-          <Button colorScheme="teal">Вернуться на главную страницу</Button>
-        </Link>
-      </Box>
+    <Container pt={120}>
+      <Section delay={0.1} mb={0}>
+        <Heading as="h3" fontSize={20} mb={4}>
+          Ошибка 404 - такой страницы не существует! :с
+        </Heading>
+      </Section>
+
+      <Section delay={0.2} mb={0}>
+        <Box align="center" my={10}>
+          <Link to="/" variant="hover-no">
+            <Button colorScheme="teal">Вернуться на главную страницу</Button>
+          </Link>
+        </Box>
+      </Section>
     </Container>
   );
 }
